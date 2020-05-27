935159e55cfd79f410fa46f9bf71353431f8ffc8
Daniel Díaz Torres <ddtorres055@gmail.com>
2020-05-27 03:06:33 -0500

    Fix mysql connection

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ session_start();
 $connection = mysqli_connect(
     'b495kugiwuth2ayq9xh1-mysql.services.clever-cloud.com',
     'ueztspeam3jbvwg5',
-    'ueztspeam3jbvwg5',
+    'ZCSUTtCQZP9j3ZVcbNiG',
     'b495kugiwuth2ayq9xh1'
 );
 
